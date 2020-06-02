c6c416ed9ffa72c650808f29bfa2c176c7ef8b47
tony <wabslygzj@163.com>
2020-06-02 13:10:04 +0800

    Add no args constructor function.

--- src/main/java/com/rainbow/datapermission/plugin/request/SQLRequest.java
+++ src/main/java/com/rainbow/datapermission/plugin/request/SQLRequest.java
@@ -13,6 +13,9 @@ public final class SQLRequest implements Serializable {
     private String[] ignoreChildren;
     private String[] ignoreParams;
 
+    public SQLRequest() {
+
+    }
 
     public SQLRequest(String sql, Map<String, String> params, String[] ignoreChildren, String[] ignoreParams) {
         this.sql = sql;
@@ -21,28 +24,12 @@ public final class SQLRequest implements Serializable {
         this.ignoreParams = ignoreParams;
     }
 
-    public void setIgnoreChildren(String[] ignoreChildren) {
-        this.ignoreChildren = ignoreChildren;
-    }
-
-    public String[] getIgnoreChildren() {
-        return ignoreChildren;
-    }
-
-    public void setIgnoreParams(String[] ignoreParams) {
-        this.ignoreParams = ignoreParams;
-    }
-
-    public String[] getIgnoreParams() {
-        return ignoreParams;
+    public String getSql() {
+        return sql;
     }
 
     public void setSql(String sql) {
         this.sql = sql;
-    }
-
-    public String getSql() {
-        return sql;
     }
 
     public Map<String, String> getParams() {
@@ -51,5 +38,21 @@ public final class SQLRequest implements Serializable {
 
     public void setParams(Map<String, String> params) {
         this.params = params;
+    }
+
+    public String[] getIgnoreChildren() {
+        return ignoreChildren;
+    }
+
+    public void setIgnoreChildren(String[] ignoreChildren) {
+        this.ignoreChildren = ignoreChildren;
+    }
+
+    public String[] getIgnoreParams() {
+        return ignoreParams;
+    }
+
+    public void setIgnoreParams(String[] ignoreParams) {
+        this.ignoreParams = ignoreParams;
     }
 }
